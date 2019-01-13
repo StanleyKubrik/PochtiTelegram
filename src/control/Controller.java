075ec93e53ba8ebec5bc06309fe16664f6248c49
StanleyKubrik <sukhonosov.nikita@gmail.com>
2019-01-13 21:20:46 +0200

    Chat v0.2 -Add TODO

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -3,28 +3,30 @@ package control;
 import io.reactivex.Observable;
 
 public class Controller implements IController {
+
     @Override
     public void login() {
-
+        // TODO
     }
 
     @Override
     public void chat() {
-
+        // TODO
     }
 
     @Override
     public void send(String message) {
-
+        // TODO
     }
 
     @Override
     public Observable<String> response() {
+        // TODO
         return null;
     }
 
     @Override
     public void disconnect() {
-
+        // TODO
     }
 }
--- src/model/ChatIO.java
+++ src/model/ChatIO.java
@@ -5,21 +5,22 @@ import io.reactivex.Observable;
 public class ChatIO implements IChatIO {
     @Override
     public void connect(String login) {
-
+        // TODO
     }
 
     @Override
     public void send(String message) {
-
+        // TODO
     }
 
     @Override
     public Observable<String> response() {
+        // TODO
         return null;
     }
 
     @Override
     public void disconnect() {
-
+        // TODO
     }
 }
